fac1e9fc479efc04fc2a448d7130fd9a1b995e9a
Arammaen <ajqualix@gmail.com>
2022-09-03 01:26:06 -0400

    removed a console log

--- client/youtubeVideoDownload.js
+++ client/youtubeVideoDownload.js
@@ -1,9 +1,13 @@
+
+
+
 window.onload = () => {
 
 	const youtubeDownloadForm = document.querySelector(".youtube-download-form");
 	const input = youtubeDownloadForm.querySelector("Input[type='text']");
 	const button = youtubeDownloadForm.querySelector("Button[type='submit']");
 	const videoInfo = document.querySelector(".video-info-block");
+
 	button.addEventListener("click", (e)=>{
 		const url = input.value;
 		if(youtube_parser(url)){
@@ -30,16 +34,18 @@ function youtube_parser(url){
     var match = url.match(regExp);
     return (match&&match[7].length==11)? match[7] : false;
 }
-function generateDom(data){
-    console.log(data);
 
+function generateDom(data){
+    //container is the main wrapper for results
 	const container = document.createElement("div");
     container.style.width = "100%";
+    //info is the wrapper around video info, including thumbnail and other info
     const info = document.createElement("div");
     info.style.display = "flex";
     info.style.justifyContent = "space-between";
+    //info container holds title and channel name
     const infoContainer = document.createElement("div");
-
+    //video name and link
     const titleLink = document.createElement("a")
     titleLink.href = `https://www.youtube.com/watch?v=${data.videoDetails.videoId}`;
     titleLink.target = "_blank";
@@ -48,7 +54,7 @@ function generateDom(data){
     infoTitle.style.textAlign = "left";
     titleLink.appendChild(infoTitle);
     infoContainer.appendChild(titleLink);
-
+    //video poster channel and link
     const authorLink = document.createElement("a")
     authorLink.href = `https://www.youtube.com/channel/${data.videoDetails.channelId}`;
     authorLink.target = "_blank";
@@ -58,24 +64,23 @@ function generateDom(data){
     authorLink.appendChild(infoAuthor);
     infoContainer.appendChild(authorLink);
     infoContainer.style.paddingRight = "30px";
-
-
+    //thumbnail
     const thumb = document.createElement("img");
     thumb.src = data.videoDetails.thumbnail.thumbnails[data.videoDetails.thumbnail.thumbnails.length - 1].url;
     thumb.style.maxWidth = "30%";
     info.appendChild(infoContainer);
     info.appendChild(thumb)
-
+    //container to hold all download links
 	const ul = document.createElement("ul");
     ul.style.padding = "0px";
     ul.style.display = "flex";
     ul.style.justifyContent = "space-between";
     ul.style.margin = "30px 0px";
+    //loop to display each resolution download link
 	data.streamingData.formats.forEach(v=>{
 		const li = document.createElement("li");
         li.style.listStyle = "none";
         li.style.width = "30%";
-
 		const a = document.createElement("a");
 		a.href = v.url;
 		a.innerHTML = v.qualityLabel;
@@ -91,7 +96,5 @@ function generateDom(data){
 	});
     container.appendChild(info);
 	container.appendChild(ul);
-
 	return container
-
 }
